569e93c51406ccd323c8aa1c9b392c8b8d4bfa52
Danilo <dansousac2@gmail.com>
2021-04-17 21:37:49 -0300

    mudança da pasta de icones

--- src/Imagens.java
+++ src/Imagens.java
@@ -3,23 +3,23 @@ import javax.swing.ImageIcon;
 public class Imagens {
 	
 	public static final ImageIcon ICONE_LOGOMARCA = 
-			new ImageIcon(Imagens.class.getResource("/Livraria/logo.png"));
+			new ImageIcon(Imagens.class.getResource("/logo.png"));
 	
 	public static final ImageIcon ICONE_PESQUISAR = 
-			new ImageIcon(Imagens.class.getResource("/Livraria/pesquisar.png"));
+			new ImageIcon(Imagens.class.getResource("/pesquisar.png"));
 	
 	public static final ImageIcon ICONE_COLECAO = 
-			new ImageIcon(Imagens.class.getResource("/Livraria/colecao.png"));
+			new ImageIcon(Imagens.class.getResource("/colecao.png"));
 	
 	public static final ImageIcon ICONE_USUARIO = 
-			new ImageIcon(Imagens.class.getResource("/Livraria/usuario.png"));
+			new ImageIcon(Imagens.class.getResource("/usuario.png"));
 	
 	public static final ImageIcon ICONE_LIVREIRO = 
-			new ImageIcon(Imagens.class.getResource("/Livraria/livreiro.png"));
+			new ImageIcon(Imagens.class.getResource("/livreiro.png"));
 	
 	public static final ImageIcon ICONE_HOMEPAGE = 
-			new ImageIcon(Imagens.class.getResource("/Livraria/homepage.png"));
+			new ImageIcon(Imagens.class.getResource("/homepage.png"));
 	
 	public static final ImageIcon ICONE_COLECAO_ACESSO = 
-			new ImageIcon(Imagens.class.getResource("/Livraria/colecaoAcesso.png"));
+			new ImageIcon(Imagens.class.getResource("/colecaoAcesso.png"));
 }
