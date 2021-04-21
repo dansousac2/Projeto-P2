bb06254aefd8e5a1ffb71d4ab34aad64c39f416e
LeonardoFreitasADS <leonardo.lucena@academico.ifpb.edu.br>
2021-04-20 22:43:49 -0300

    Adicionei um comentario

--- src/MainParaTestes.java
+++ src/MainParaTestes.java
@@ -2,7 +2,7 @@
 public class MainParaTestes {
 
 	public static void main(String[] args) {
-		
+		// Testando git
 		Usuario user01 = new Usuario("Danilo", "dansousac2@gmail.com",
 				"0090");
 		user01.setEhLivreiro(false); // TESTANDO BOTÃO HOME PAGE
