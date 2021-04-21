a422b55a3875fd6adb79d2c33142a894d8ebe903
LeonardoFreitasADS <82853605+LeonardoFreitasADS@users.noreply.github.com>
2021-04-20 22:54:00 -0300

    removi o comentário

--- src/MainParaTestes.java
+++ src/MainParaTestes.java
@@ -2,10 +2,9 @@
 public class MainParaTestes {
 
 	public static void main(String[] args) {
-		// Testando git
 		Usuario user01 = new Usuario("Danilo", "dansousac2@gmail.com",
 				"0090");
-		user01.setEhLivreiro(false); // TESTANDO BOT√O HOME PAGE
+		user01.setEhLivreiro(false); // TESTANDO BOT√ÉO HOME PAGE
 		
 		new ListarLivrosUsuario(user01);
 //		new ColecaoUsuario(user01);
