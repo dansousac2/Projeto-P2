50f22ed659e766d3805e6bc21bd97b58856bd43a
Danilo <dansousac2@gmail.com>
2021-04-25 15:11:30 -0300

    adicao de atributo

--- src/testeMain.java
+++ src/testeMain.java
@@ -3,19 +3,21 @@ public class testeMain {
 	
 	public static void main(String[] args) {
 		
-//		Usuario novoUsuario = new Usuario("Danilo S. Costa", "dansousac2@hotmail.com", "123qwe");
+		Usuario novoUsuario = new Usuario("Danilo S. Costa", "dansousac2@hotmail.com", "123qwe");
+//		
+		new ListarLivrosUsuario(novoUsuario);
+		
 //		novoUsuario.setEhLivreiro(true);
-//		new ListarLivrosUsuario(novoUsuario);
-
 //		new ListarLivrosLivreiro(novoUsuario);
+
 		
 //		new ColecaoUsuario(novoUsuario);
-		try {
-			
-			new JanelaLoguin(new PersistenciaLivros().recuperarCentral("Dados_Livraria.xml"));
-		}catch (Exception e) {
-			// TODO: handle exception
-		}
+//		try {
+//			
+//			new JanelaLoguin(new PersistenciaLivros().recuperarCentral("Dados_Livraria.xml"));
+//		}catch (Exception e) {
+//			// TODO: handle exception
+//		}
 		
 		
 	}
